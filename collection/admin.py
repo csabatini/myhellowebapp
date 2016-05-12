71b062f151cb8416b9561dfdae850f5fad140612
Caleb Sabatini <calebsabatini@gmail.com>
2016-05-11 23:39:07 -0500

    collection model

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from collection.models import Stock
 
-# Register your models here.
+
+class StockAdmin(admin.ModelAdmin):
+    model = Stock
+    list_display = ('symbol', 'name',)
+    prepopulated_fields = {'slug': ('symbol',)}
+
+admin.site.register(Stock, StockAdmin)
---
+++ collection/migrations/0001_initial.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Stock',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('symbol', models.CharField(max_length=255)),
+                ('name', models.CharField(max_length=255)),
+                ('slug', models.SlugField(unique=True)),
+            ],
+        ),
+    ]
