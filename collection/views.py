2392ed0b02e898b9e1af7b05fed69967ae3bc5d4
Caleb Sabatini <calebsabatini@gmail.com>
2016-05-12 22:57:59 -0500

    pass template variable from database

--- collection/views.py
+++ collection/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from collection.models import Stock
 
 
 def index(request):
     number = 6
+    stocks = Stock.objects.all()
     return render(request, 'index.html', {
         'number': number,
+        'stocks': stocks,
     })
