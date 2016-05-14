f755c2787fddb4a0c392039acff9e82a559dcf4f
Caleb Sabatini <calebsabatini@gmail.com>
2016-05-13 23:53:21 -0500

    password change/reset

--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -1,6 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
+from django.contrib.auth.views import (
+    password_reset,
+    password_reset_done,
+    password_reset_confirm,
+    password_reset_complete
+)
 
 urlpatterns = patterns('',
     url(r'^$', 'collection.views.index', name='home'),
@@ -9,5 +15,13 @@ urlpatterns = patterns('',
     url(r'^stocks/(?P<slug>[-\w]+)/$', 'collection.views.stock_detail', name='stock_detail'),
     url(r'^stocks/(?P<slug>[-\w]+)/edit/$', 'collection.views.edit_stock', name='edit_stock'),
     url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^accounts/password/reset/$', password_reset,
+        {'template_name':'registration/password_reset_form.html'}, name='password_reset'),
+    url(r'^accounts/password/reset/done/$', password_reset_done,
+        {'template_name':'registration/password_reset_done.html'}, name='password_reset_done'),
+    url(r'^accounts/password/reset/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', password_reset_confirm,
+        {'template_name': 'registration/password_reset_confirm.html'}, name='password_reset_confirm'),
+    url(r'^accounts/password/done/$', password_reset_complete,
+        {'template_name': 'registration/password_reset_complete.html'}, name='password_reset_complete'),
     url(r'^admin/', include(admin.site.urls)),
 )
