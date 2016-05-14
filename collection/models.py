1cb935a953f0233121e053ff8cc125157b9dffcb
Caleb Sabatini <calebsabatini@gmail.com>
2016-05-13 21:29:55 -0500

    detail url, view, template

--- collection/models.py
+++ collection/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class Stock(models.Model):
         symbol = models.CharField(max_length=255)
         name = models.CharField(max_length=255)
--- collection/views.py
+++ collection/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from collection.models import Stock
 
-
 def index(request):
     number = 6
     stocks = Stock.objects.all()
     return render(request, 'index.html', {
         'number': number,
         'stocks': stocks,
+    })
+
+def stock_detail(request, slug):
+    stock = Stock.objects.get(slug=slug)
+    return render(request, 'stocks/stock_detail.html', {
+        'stock': stock,
     })
